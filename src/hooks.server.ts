cc3db9c93e23d97800967eb300ea35aa457950ca
techswarn <tech.swarns@gmail.com>
2024-03-05 20:50:06 +0530

    code update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,10 @@
 import { redirect } from "@sveltejs/kit";
 
 export async function handle({ event, resolve }) {
-  console.log(event);
-  console.log(resolve);
+  console.log(event.url.host);
   // if route matches "/banana" return banana
   if (event.url.host == "techenv.dev") {
-    throw redirect(301, "https://www.techenv.dev");
+    throw redirect(301, "www.techenv.dev");
   }
 
   // otherwise use the default behavior
