8b399ba1059d8f8bf918c37284db25f431e18767
techswarn <tech.swarns@gmail.com>
2024-03-06 15:53:52 +0530

    code update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,7 +6,7 @@ export async function handle({ event, resolve }) {
     console.log("Thats a match");
     throw redirect(301, "https://techenv.dev");
   }
-  if (event.url.pathname.startsWith("/custom")) {
+  if (event.url.pathname.startsWith("/")) {
     return new Response("custom response");
   }
 
